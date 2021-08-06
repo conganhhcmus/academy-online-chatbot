57c2d840af780708af6ab44ec285a40004a09fb5
LÊ ĐOÀN CÔNG ẢNH <conganhhcmus@gmail.com>
2021-08-06 13:58:18 +0700

    [check] - [check dialogflow]

--- services/chatbot.js
+++ services/chatbot.js
@@ -3,18 +3,19 @@ const dialogflowAPI = require('./../api/dialogflow');
 
 module.exports = {
     handleMessage: async (sender_psid, received_message) => {
-        // // Check if the message contains text
-        // if (received_message.text) {
-        //     // Create the payload for a basic text message
-        //     response = {
-        //         text: `You sent the message: "${received_message.text}". Now send me an image!`,
-        //     };
-        // }
+        // Check if the message contains text
+        if (received_message.text) {
+            // Create the payload for a basic text message
+            response = {
+                text: `You sent the message: "${received_message.text}". Now send me an image!`,
+            };
+        }
 
-        let response = await dialogflowAPI.fullfilMessage(
+        let test = await dialogflowAPI.fullfilMessage(
             sender_psid,
             received_message.text
         ).received_message;
+        console.log(test);
 
         // Sends the response message
         messengerAPI.callSendAPI(sender_psid, response);
