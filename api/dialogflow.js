38e2a3e1803c51ddc1242d9acc2541af14ddda98
LÊ ĐOÀN CÔNG ẢNH <conganhhcmus@gmail.com>
2021-08-06 14:36:31 +0700

    [fix] - [fix dialogflow sessionClient]

--- api/dialogflow.js
+++ api/dialogflow.js
@@ -1,6 +1,7 @@
 // You can find your project ID in your Dialogflow agent settings
 const projectId = 'newagent-mivp'; //https://dialogflow.com/docs/agents#settings
-const sessionId = '123456';
+const uuid = require('uuid');
+const sessionId = uuid.v4();
 const languageCode = 'en-US';
 
 const dialogflow = require('dialogflow');
@@ -17,7 +18,7 @@ const sessionClient = new dialogflow.SessionsClient(config);
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
 module.exports = {
-    fullfilMessage: (userId, message) => {
+    fullfilMessage: async (message) => {
         const request = {
             session: sessionPath,
             queryInput: {
@@ -28,14 +29,16 @@ module.exports = {
             },
         };
 
-        sessionClient
-            .detectIntent(request)
-            .then((responses) => {
-                const result = responses[0].queryResult;
-                console.log(userId, result.fulfillmentText);
-            })
-            .catch((err) => {
-                console.error('ERROR:', err);
-            });
+        // Send request and log result
+        const responses = await sessionClient.detectIntent(request);
+        console.log('Detected intent');
+        const result = responses[0].queryResult;
+        console.log(`  Query: ${result.queryText}`);
+        console.log(`  Response: ${result.fulfillmentText}`);
+        if (result.intent) {
+            console.log(`  Intent: ${result.intent.displayName}`);
+        } else {
+            console.log(`  No intent matched.`);
+        }
     },
 };
