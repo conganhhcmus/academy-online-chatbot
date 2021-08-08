bfd77fb927de9473d02037262c334ea0013d30c1
LÊ ĐOÀN CÔNG ẢNH <conganhhcmus@gmail.com>
2021-08-08 11:19:10 +0700

    [delete] - [delte trash code]

--- src/services/chatbot.js
+++ src/services/chatbot.js
@@ -56,11 +56,6 @@ module.exports = {
         let payload = received_postback.payload;
 
         // Set the response based on the postback payload
-        if (payload === 'yes') {
-            response = { text: 'Thanks!' };
-        } else if (payload === 'no') {
-            response = { text: 'Oops, try sending another image.' };
-        }
         switch (payload) {
             case PAYLOAD.GET_STARTED:
                 response = { text: 'Welcome to Academy Online Learning!' };
