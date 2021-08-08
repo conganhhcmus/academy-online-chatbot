41af1d2a79a56f5f104873044b5a7851d318903a
LÊ ĐOÀN CÔNG ẢNH <conganhhcmus@gmail.com>
2021-08-08 12:20:24 +0700

    [fix] - [fix flow setup & remove profile messenger platform]

--- src/app.js
+++ src/app.js
@@ -7,12 +7,19 @@ const config = require('./config');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Remove profiler
-require('./api/messenger').removeProfile();
-// Setup profile
-require('./api/messenger').setUpProfile();
-
 app.get('/', (req, res) => {
+    res.send('Hello Word!');
+});
+
+app.get('/remove', (req, res) => {
+    // Remove profiler
+    require('./api/messenger').removeProfile();
+    res.send('Hello Word!');
+});
+
+app.get('/setup', (req, res) => {
+    // Setup profile
+    require('./api/messenger').setUpProfile();
     res.send('Hello Word!');
 });
 
